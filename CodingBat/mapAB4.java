0c6edfe2ff4afcc666a4b2fcb292629b9b360de2
Alzahra Amur <alzahraamur11@gmail.com>
2025-07-28 21:48:40 +0400

    Add Map-1files.java

---
+++ CodingBat/mapAB4.java
@@ -0,0 +1,22 @@
+import java.util.Map;
+
+public class mapAB4 {
+    public Map<String, String> mapAB4(Map<String, String> map) {
+        if (map.containsKey("a") && map.containsKey("b")) {
+            String aVal= map.get("a");
+            String bVal= map.get("b");
+
+            if (aVal.length()> bVal.length()) {
+                map.put ("c", aVal);
+            }
+            else if (bVal.length() >aVal.length()) {
+                map.put ("c", bVal);
+            }
+            else {
+                map.put("a","");
+                map.put ("b","");
+            }
+        }
+        return map;
+    }
+}
